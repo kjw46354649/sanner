1879502e7956eca2bb066d9bd0de04c93f3e7754
heekyu.kim <heekyu.kim@innodale.com>
2020-05-07 14:41:06 +0900

    .

---
+++ src/main/java/com/jmes/service/impl/EstimateServiceImpl.java
@@ -0,0 +1,44 @@
+package com.jmes.service.impl;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jmes.dao.EstimateDao;
+import com.jmes.service.EstimateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class EstimateServiceImpl implements EstimateService {
+
+    @Autowired
+    public EstimateDao estimateDao;
+
+    @Override
+    public void registerEstimateOrder(HashMap<String, Object> hashMap) throws Exception {
+        String jsonObject = (String) hashMap.get("data");
+        ObjectMapper objectMapper = new ObjectMapper();
+        ArrayList<Map<String, Object>> jsonMap = null;
+        Map<String, Object> data = new HashMap<String, Object>();
+
+        if (jsonObject != null) {
+            jsonMap = objectMapper.readValue(jsonObject, new TypeReference<ArrayList<Map<String, Object>>>() {
+            });
+        }
+
+        for(int i=0; i<jsonMap.size(); i++) {
+            data = jsonMap.get(i);
+            if(i == 0) {
+                estimateDao.updateEstimateMasterFinish(data);
+            }
+
+            estimateDao.insertEstimateOrderControlMaster(data);
+            estimateDao.insertEstimateOrderControlDetail(data);
+            estimateDao.insertEstimateOrderControlOrder(data);
+        }
+
+    }
+}
