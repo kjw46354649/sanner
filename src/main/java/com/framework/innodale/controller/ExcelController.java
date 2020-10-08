f9130b1e1b8c57c5f0c62e7281f1eb86120c1cfc
hyungseokno <hyoungs.no@innodale.com>
2020-10-08 18:01:03 +0900

    Excel Export 삭제 처리

---
+++ src/main/java/com/framework/innodale/controller/ExcelController.java
@@ -0,0 +1,238 @@
+package com.framework.innodale.controller;
+
+import com.framework.innodale.component.CommonUtility;
+import com.framework.innodale.service.InnodaleService;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+import net.sf.jxls.transformer.XLSTransformer;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.*;
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.List;
+
+@Controller
+public class ExcelController {
+
+    @Autowired
+    private Environment environment;
+
+    @Autowired
+    private InnodaleService innodaleService;
+
+    @RequestMapping("/downloadExcel")
+    public void excelDownLoad(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException, Exception {
+        HashMap<String, Object> paramMap = new HashMap<String, Object>();
+        HashMap<String, Object> map = new HashMap<String, Object>();
+
+        List<Map<String, Object>> dataList = null;
+
+        String excelDir = environment.getRequiredProperty(CommonUtility.getServerType() + ".base.excel.template.path");
+
+        InputStream is = null;
+        Workbook workbook = null;
+        FileOutputStream fos = null;
+        FileInputStream fileInputStream = null;
+
+        try {
+
+            String sqlIds = req.getParameter("sqlId");
+            String paramName = req.getParameter("paramName");
+            String paramData = req.getParameter("paramData");
+            String mapInputId = req.getParameter("mapInputId");
+            String templateFileName = req.getParameter("template");
+
+            SimpleDateFormat time = new SimpleDateFormat("yyyyMMddhhmmss");
+            Date today = new Date();
+            String date = time.format(today);
+
+            String[] arrayQueryId = sqlIds.split(":");
+            String[] arrayParamName = paramName.split(":");
+            String[] arrayParams = paramData.split(":");
+            String[] arrayMapInputIds = mapInputId.split(":");
+
+            // AllData input Map
+            for (int i = 0; i < arrayParamName.length; i++) {
+                paramMap.put(arrayParamName[i], arrayParams[i]);
+            }
+
+            for(int j = 0; j < arrayQueryId.length; j++) {
+                paramMap.put("queryId", arrayQueryId[j]);
+
+                dataList = innodaleService.getList(paramMap);
+                map.put(arrayMapInputIds[j], dataList);
+            }
+
+            is = new BufferedInputStream(new FileInputStream(excelDir + File.separator + templateFileName + ".xlsx"));
+            XLSTransformer xls = new XLSTransformer();
+            workbook = xls.transformXLS(is, map);
+
+            res.setHeader("Content-Disposition", CommonUtility.getDisposition(templateFileName + date + ".xlsx", CommonUtility.getBrowser(req)));
+            res.setHeader("Content-Transfer-Encoding", "binary");
+            res.setContentType("application/x-msexcel");
+            res.setContentType("application/octet-stream; charset=utf-8");
+            workbook.write(res.getOutputStream());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (is != null) is.close();
+            if (workbook != null) workbook.close();
+            if (fos != null) fos.close();
+            if (fileInputStream != null) fileInputStream.close();
+        }
+    }
+
+
+
+    @RequestMapping("/itemOrderRegisterOrderSheetPrint")
+    public void itemOrderRegisterOrderSheetPrint(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException, Exception {
+        HashMap<String, Object> paramMap = new HashMap<String, Object>();
+
+        List<HashMap<String, Object>> sheetMaps = new ArrayList<HashMap<String, Object>>();
+        List<String> sheetNames = new ArrayList<>();
+        List<Map<String, Object>> dataList = null;
+        List<Map<String, Object>> infoList = null;
+
+        String excelDir = environment.getRequiredProperty(CommonUtility.getServerType() + ".base.excel.template.path");
+
+        InputStream is = null;
+        Workbook workbook = null;
+        FileOutputStream fos = null;
+        FileInputStream fileInputStream = null;
+        XLSTransformer transformer = new XLSTransformer();
+
+        try {
+
+            String sqlIds = req.getParameter("sqlId");
+            String paramName = req.getParameter("paramName");
+            String paramData = req.getParameter("paramData");
+            String mapInputId = req.getParameter("mapInputId");
+            String templateFileName = req.getParameter("template");
+
+            SimpleDateFormat time = new SimpleDateFormat("yyyyMMddhhmmss");
+            Date today = new Date();
+            String date = time.format(today);
+
+            String[] arrayQueryId = sqlIds.split(":");
+            String[] arrayParamName = paramName.split(":");
+            String[] arrayParams = paramData.split(":");
+            String[] arrayMapInputIds = mapInputId.split(":");
+
+            is = new BufferedInputStream(new FileInputStream(excelDir + File.separator + templateFileName + ".xlsx"));
+            XLSTransformer xls = new XLSTransformer();
+
+            // AllData input Map
+            for (int i = 0; i < arrayParamName.length; i++) {
+                paramMap.put(arrayParamName[i], arrayParams[i]);
+            }
+
+            // 정보성 쿼리.
+            paramMap.put("queryId", "material.selectItemOrderRegisterPopTable");
+            infoList = innodaleService.getList(paramMap);
+            for(int j=0; j < infoList.size(); j++) {
+                HashMap<String, Object> map = new HashMap<String, Object>();
+                sheetNames.add((String) infoList.get(j).get("MATERIAL_COMP_NM"));
+
+                paramMap.put("queryId", "excel.selectItemOrderRegisterOrderSheetListExcel");
+                paramMap.put("CONCAT_SEQ", infoList.get(j).get("CONCAT_SEQ"));
+                dataList = innodaleService.getList(paramMap);
+                map.put("MATERIAL_ORDER_NUM", dataList.get(0).get("MATERIAL_ORDER_NUM"));
+                map.put("ORDER_DT", dataList.get(0).get("ORDER_DT"));
+                map.put("ORDER_COMP_NM", dataList.get(0).get("ORDER_COMP_NM"));
+                map.put("data", dataList);
+                sheetMaps.add(map);
+            }
+
+            workbook = transformer.transformMultipleSheetsList(is, sheetMaps, sheetNames, "data", new HashMap(), 0);
+
+            File file = new File(excelDir + File.separator + "excel");
+            file.mkdirs();
+            fos = new FileOutputStream(new File(excelDir + File.separator + "excel" + File.separator + templateFileName + date + ".xlsx"));
+            workbook.write(fos);
+            fos.close();
+
+            // System.out.println(excelDir + File.separator + templateFileName + date + ".xlsx");
+
+            /*res.setHeader("Content-Disposition", CommonUtility.getDisposition(templateFileName + date + ".xlsx", CommonUtility.getBrowser(req)));
+            res.setHeader("Content-Transfer-Encoding", "binary");
+            res.setContentType("application/x-msexcel");
+            res.setContentType("application/octet-stream; charset=utf-8");
+            workbook.write(res.getOutputStream());*/
+
+
+            //First we read the Excel file in binary format into FileInputStream
+            FileInputStream input_document = new FileInputStream(new File(excelDir + File.separator + "excel" + File.separator + templateFileName + date + ".xlsx"));
+            // Read workbook into HSSFWorkbook
+            XSSFWorkbook my_xls_workbook = new XSSFWorkbook(input_document);
+            // Read worksheet into HSSFSheet
+            XSSFSheet my_worksheet = my_xls_workbook.getSheetAt(0);
+            // To iterate over the rows
+            Iterator<Row> rowIterator = my_worksheet.iterator();
+            //We will create output PDF document objects at this point
+            Document iText_xls_2_pdf = new Document();
+            File pdfFile = new File(excelDir + File.separator + "pdf");
+            pdfFile.mkdirs();
+            PdfWriter.getInstance(iText_xls_2_pdf, new FileOutputStream(new File(excelDir + File.separator + "pdf" + File.separator + templateFileName + date + ".pdf")));
+            iText_xls_2_pdf.open();
+            //we have two columns in the Excel sheet, so we create a PDF table with two columns
+            //Note: There are ways to make this dynamic in nature, if you want to.
+            PdfPTable my_table = new PdfPTable(10);
+            //We will use the object below to dynamically add new data to the table
+            PdfPCell table_cell;
+            //Loop through rows.
+            while(rowIterator.hasNext()) {
+                Row row = rowIterator.next();
+                Iterator<Cell> cellIterator = row.cellIterator();
+                while(cellIterator.hasNext()) {
+                    Cell cell = cellIterator.next(); //Fetch CELL
+                    switch(cell.getCellType()) { //Identify CELL type
+                        //you need to add more code here based on
+                        //your requirement / transformations
+                        case Cell.CELL_TYPE_STRING:
+                            //Push the data from Excel to PDF Cell
+                            table_cell=new PdfPCell(new Phrase(cell.getStringCellValue()));
+                            //feel free to move the code below to suit to your needs
+                            my_table.addCell(table_cell);
+                            break;
+                    }
+                    //next line
+                }
+
+            }
+            //Finally add the table to PDF document
+            iText_xls_2_pdf.add(my_table);
+            iText_xls_2_pdf.close();
+            //we created our pdf file..
+            input_document.close(); //close xls
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (is != null) is.close();
+            if (workbook != null) workbook.close();
+            if (fos != null) fos.close();
+            if (fileInputStream != null) fileInputStream.close();
+        }
+    }
+}
